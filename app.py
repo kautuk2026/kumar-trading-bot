54141bd86088aa59f9b16c06e12b46be9e15d9c6
kautuk2026 <kumaryashwantrao@gmail.com>
2026-06-23 16:50:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ import os
 
 app = Flask(__name__)
 
-# ✅ correct environment variables
 BOT_TOKEN = os.environ.get("8843764106:AAET5Nlvp-lcWQKHRsCOzgIgQNKqslObl2o")
 CHAT_ID = os.environ.get("917384025")
 
@@ -15,7 +14,6 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-
     message = data.get("message", "🚀 New Trading Signal")
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -26,8 +24,4 @@ def webhook():
     }
 
     requests.post(url, json=payload)
-
     return {"status": "success"}
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
